24a165ee4c1e333f1047fe02d3f887208e007d58
Nai-Ha <89713168+Nai-Ha@users.noreply.github.com>
2022-11-01 19:37:13 +0700

    THuchanh Buoi 5

---
+++ THUC_HANH_buoi_5/bai1/b1.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+#define Max_N 100
+#define OO 9999
+typedef int ElementType;
+int parent[Max_N];
+typedef struct{
+	int u,v,w;
+}Edge;
+
+typedef struct {
+	int n,m;
+	Edge Edges[MAX_SIZE];
+} Graph;
+
+typedef struct {
+	int data[Max_N];
+	int size;
+} List;
+
+typedef struct {
+	int data[Max_N];
+	int front;
+	int Rear;
+} Queue;
+
+// <!------ List -------!>
+void make_null(List *pL) {
+	pL->size = 0;
+}
+
+void push_back(List *pL,int x) {
+	pL->data[pL->size] = x;
+	pL->size++;
+}
+
+int element_at(List *pL,int p) {
+	return pL->data[p-1];
+}
+
+void printList(List *pL) {
+	int i;
+	for(i=0; i<pL->size; i++)
+		printf("%d ",pL->data[i]);
+	printf("\n");
+}
+// <!------ List -------!>
+
+// <!------ Queue -------!>
+void make_null_queue(Queue *pQ) {
+	pQ->front = 0;
+	pQ->Rear = -1;
+}
+
+void enQueue(Queue *pQ,int u) {
+	pQ->Rear++;
+	pQ->data[pQ->Rear] = u;
+
+}
+
+void deQueue(Queue *pQ) {
+	pQ->front++;
+}
+
+ElementType front(Queue *pQ) {
+	return pQ->data[pQ->front];
+}
+
+int emptyQueue(Queue *pQ) {
+	return pQ->front > pQ->Rear;
+}
+// <!------ Queue -------!>
+
+// <!------ Graph -------!>
+void init_graph(Graph *pG,int n) {
+	pG->n = n;
+	pG->m = 0;
+}
+
+void add_edge(Graph *pG,int x,int y,int w) {
+	pG->Edges[pG->m].u = x;
+	pG->Edges[pG->m].v = y;
+	pG->Edges[pG->m].w = w;
+	pG->m++;
+}
+
+void printGraph(Graph G) {
+	int i,j;
+	for(i=0; i<G.m; i++) 
+		printf("%d %d %d\n",G.Edges[i].u,G.Edges[i].v,G.Edges[i].w);
+	
+}
+
+int findRoot(int u){
+	while(parent[u] != u)
+		u = parent[u];
+	return u;
+}
+
+void sortEdge(Graph *pG){
+	int i,j;
+	for(i=0;i<pG->m;i++)
+		for(j=i+1;j<pG->m;j++)
+			if(pG->Edges[i].w > pG->Edges[j].w){
+				Edge temp = pG->Edges[i];
+				pG->Edges[i] = pG->Edges[j];
+				pG->Edges[j] = temp;
+			}
+}
+
+int Kruskal(Graph *pG,Graph *pT){
+	// Sap xep cung theo thu tu tang 
+	sortEdge(pG);
+	
+	// Khoi tao cay khung khong chua cung nao,khoi tao BPLT
+	init_graph(pT,pG->n);
+	int u,e;
+	for(u=1;u<=pG->n;u++)
+		parent[u] = u;
+	int sum_w = 0;
+	
+	// Duyet qua cac cung cua G
+	for(e=0;e<pG->m;e++){
+		int x = pG->Edges[e].u;
+		int y = pG->Edges[e].v;
+		int t = pG->Edges[e].w;
+		int root_x = findRoot(x);
+		int root_y = findRoot(y);
+		
+		if(root_x != root_y){
+			// Them cung
+			if(x < y)
+				add_edge(pT,x,y,t);
+			else 
+				add_edge(pT,y,x,t);
+			
+			// Gop 2 BPLT root_x va root_y
+			parent[root_y] = root_x;
+			sum_w += t;
+		}
+ 	}
+ 	return sum_w;
+}
+
+int main() {
+	Graph G,T;
+	int n,m,u,v,i,j,w;
+	freopen("test.txt","r",stdin);
+	scanf("%d%d",&n,&m);
+	init_graph(&G,n);
+	for(i=1;i<=m;i++){
+		scanf("%d%d%d",&u,&v,&w);
+//		printf("u v w: %d %d %d\n",u,v,w);
+		add_edge(&G,u,v,w);
+	}
+	printGraph(G);
+	
+	int sum_w = Kruskal(&G,&T);
+	printf("%d\n",sum_w);
+	for(i=0;i<T.m;i++)
+		printf("%d %d %d\n",T.Edges[i].u,T.Edges[i].v,T.Edges[i].w);
+		
+	return 0;
+}
---
+++ THUC_HANH_buoi_5/bai2/b1.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+#define Max_N 100
+#define OO 9999
+#define NO_EDGE 0
+typedef int ElementType;
+int p[Max_N];
+int pi[Max_N];
+int mark[Max_N];
+typedef struct {
+	int n,m;
+	int A[Max_N][Max_N];
+} Graph;
+
+typedef struct {
+	int data[Max_N];
+	int size;
+} List;
+
+typedef struct {
+	int data[Max_N];
+	int front;
+	int Rear;
+} Queue;
+
+// <!------ List -------!>
+void make_null(List *pL) {
+	pL->size = 0;
+}
+
+void push_back(List *pL,int x) {
+	pL->data[pL->size] = x;
+	pL->size++;
+}
+
+int element_at(List *pL,int p) {
+	return pL->data[p-1];
+}
+
+void printList(List *pL) {
+	int i;
+	for(i=0; i<pL->size; i++)
+		printf("%d ",pL->data[i]);
+	printf("\n");
+}
+// <!------ List -------!>
+
+// <!------ Queue -------!>
+void make_null_queue(Queue *pQ) {
+	pQ->front = 0;
+	pQ->Rear = -1;
+}
+
+void enQueue(Queue *pQ,int u) {
+	pQ->Rear++;
+	pQ->data[pQ->Rear] = u;
+
+}
+
+void deQueue(Queue *pQ) {
+	pQ->front++;
+}
+
+ElementType front(Queue *pQ) {
+	return pQ->data[pQ->front];
+}
+
+int emptyQueue(Queue *pQ) {
+	return pQ->front > pQ->Rear;
+}
+// <!------ Queue -------!>
+
+// <!------ Graph -------!>
+void init_graph(Graph *G,int n) {
+	int i,j;
+	G->n = n;
+	for(i=1; i<=n; i++)
+		for(j=1; j<=n; j++)
+			G->A[i][j] = NO_EDGE;
+}
+
+void add_edge(Graph *pG,int x,int y,int w) {
+	pG->A[x][y] = w;
+	pG->A[y][x] = w;
+}
+
+void printGraph(Graph G) {
+	int i,j;
+	for(i=1; i<=G.n; i++) {
+		for(j=1; j<=G.n; j++)
+			printf("%d ",G.A[i][j]);
+		printf("\n");
+	}
+}
+
+int Prim(Graph *pG,Graph *pT,int s){
+	int i,u,v,x;
+	
+	// Khoi tao
+	for(u=1;u<=pG->n;u++){
+		pi[u] = OO;
+		p[u] = -1;
+		mark[u] = 0;
+	}
+	
+	pi[s] = 0;
+	
+	for(i=1;i<pG->n;i++){
+		// Tim u gan voi s nhat
+		int min_dist = OO;
+		for(x=1;x<=pG->n;x++)
+			if(mark[x] == 0 && pi[x] < min_dist){
+				min_dist = pi[x];
+				u = x;
+			}
+		
+		// Danh dau da duyet
+		mark[u] = 1;
+		for(v=1;v<=pG->n;v++){
+			if(pG->A[u][v] != NO_EDGE 
+			&& pi[v] > pG->A[u][v]){
+				pi[v] = pG->A[u][v];
+				p[v] = u;	
+			}
+		}
+	}
+	
+	// Dung cay
+	init_graph(pT,pG->n);
+	int sum_w = 0;
+	for(u=1;u<=pG->n;u++){
+		if(p[u] != -1){
+			int w = pG->A[p[u]][u];
+			add_edge(pT,p[u],u,w);
+			sum_w += w;
+		}
+	}
+	return sum_w;
+}
+
+int main() {
+	Graph G,T;
+	int n,m,u,v,i,j,w;
+	freopen("test.txt","r",stdin);
+	scanf("%d%d",&n,&m);
+	init_graph(&G,n);
+	for(i=1;i<=m;i++){
+		scanf("%d%d%d",&u,&v,&w);
+		add_edge(&G,u,v,w);
+	}
+	printGraph(G);	
+	int sum_w = Prim(&G,&T,1);
+	
+	// Mang danh dau in
+	int DANHDAUIN[20][20];
+	
+	for(i=1;i<=T.n;i++)
+		for(j=1;j<=T.n;j++)
+			DANHDAUIN[i][j] = 0;
+	
+	
+	printf("%d\n",sum_w);
+	for(i=1;i<=T.n;i++){
+		for(j=1;j<=T.n;j++)
+			if(T.A[i][j] != NO_EDGE && DANHDAUIN[i][j] == 0){
+				printf("%d %d %d\n",i,j,T.A[i][j]);		
+				DANHDAUIN[j][i] = 1;
+			}
+				
+	}
+		
+//	printGraph(T);
+	return 0;
+}
